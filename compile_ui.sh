a97bd91e0fe6002277a3ddab1550d08e3077ca67
Krissada Singhakachain <46844213+OmsinKrissada@users.noreply.github.com>
2022-05-19 13:20:18 +0700

    fix compile path for mac

--- compile_ui.sh
+++ compile_ui.sh
@@ -1,2 +1,2 @@
-venv/Scripts/pyside6-rcc designer/resources.qrc -o ui/resources_rc.py
-venv/Scripts/pyside6-uic designer/main_mac.ui > ui/mainwindow_ui.py
+venv/bin/pyside6-rcc designer/resources.qrc -o ui/resources_rc.py
+venv/bin/pyside6-uic designer/main_mac.ui > ui/mainwindow_ui.py
